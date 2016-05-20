1488b7461af319921376a5f31565d68e33a19e70
mushuanli <lizlok@gmail.com>
2016-05-20 10:53:52 +0800

    a script install centos chroot env

---
+++ install_centos.sh
@@ -0,0 +1,61 @@
+
+#   install an centos chroot system
+#   this command can run in centos/debian
+#   first,
+CHROOT_DIR=/home/build/centos
+SCHROOT_NAME=c6
+SCHROOT_DESC='Centos 6 (amd64)'
+
+#   1.  recreate rpm directory
+rm -rf  $CHROOT_DIR
+mkdir -p $CHROOT_DIR/var/lib/rpm
+  
+#   2.  special rpm rebuild dir
+rpm --rebuilddb --root=$CHROOT_DIR
+  
+#   3.  download system install package
+if [ -f centos-release-6-7.el6.centos.12.3.x86_64.rpm ] ; then
+    echo "centos-release-6-7.el6.centos.12.3.x86_64.rpm file exist, skip download"
+else
+    wget http://mirrors.163.com/centos/6.7/os/x86_64/Packages/centos-release-6-7.el6.centos.12.3.x86_64.rpm
+fi
+
+echo "install base system"
+#   4.  install base system
+rpm -ivh --root=$CHROOT_DIR --nodeps centos-release-6-7.el6.centos.12.3.x86_64.rpm
+  
+echo "install rpm-build yum,run command:"
+echo "yum --installroot=$CHROOT_DIR install -y rpm-build yum"
+#   5.  install rpmbuild/yum to dest system
+yum --installroot=$CHROOT_DIR install -y rpm-build yum
+  
+echo "prepare chroot directory"
+#   6.  prepare chroot system
+mkdir -p $CHROOT_DIR/proc
+mkdir -p $CHROOT_DIR/dev
+mkdir -p $CHROOT_DIR/sys
+cp /etc/resolv.conf $CHROOT_DIR/etc/
+  
+#   7.  now you can chroot,but if you use chroot command, you need to mount --rbind /proc /sys/ /dev
+#       or you can use schroot -c $SCHROOT_NAME to run
+#chroot $CHROOT_DIR /bin/bash –l
+
+
+#   echo schroot comfig command, use these command can direct add config to schroot, then use schroot to start new system
+echo "if you use schroot, run these command in dest linux system"
+echo "
+export CHROOT_DIR=$CHROOT_DIR
+export SCHROOT_NAME='$SCHROOT_NAME'
+export SCHROOT_DESC=$SCHROOT_DESC
+(
+cat <<EOF
+[$SCHROOT_NAME]
+description=$SCHROOT_DESC
+directory=$CHROOT_DIR
+root-users=root
+root-groups=root
+type=directory
+aliases=unstable,default
+EOF
+) >> /etc/schroot/schroot.conf
+"
