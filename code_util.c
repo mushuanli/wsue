5412e68117ac79c27e7ff7cf7126f1be69f48957
mushuanli <lizlok@gmail.com>
2016-05-31 20:26:15 +0800

    add inotify, backtrace function

--- code_util.c
+++ code_util.c
@@ -4,6 +4,8 @@
  *  LOG FUNCTION:   RAIN_DBGLOG
  *
  * ---------------------------------------------------------------------------------------------------------------*/
+#define _GNU_SOURCE
+#include <dlfcn.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -11,6 +13,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
+#include <execinfo.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#ifdef __cplusplus
+#include <cxxabi.h>
+#endif
 
 #define RAINLILOG_FILENAME    "rainli.log"
 
@@ -21,7 +30,14 @@
     rainlog2file("%02d:%02d:%02d.%03d [%s:%d] "fmt,day.tm_hour,day.tm_min,day.tm_sec,now.tv_nsec/1000000,__func__,__LINE__,##arg);    \
 }while(0)
 
-void rainlog2file(const char *fmt,...)
+#define RAIN_DBGTRACE(fmt,arg...)   do{                                     \
+    struct timespec    now;    clock_gettime(CLOCK_REALTIME,&now);         \
+    struct tm day   = *localtime(&now.tv_sec);                              \
+    printf("%02d:%02d:%02d.%03d [%s:%d] "fmt,day.tm_hour,day.tm_min,day.tm_sec,now.tv_nsec/1000000,__func__,__LINE__,##arg);    \
+    Backtrace("%02d:%02d:%02d.%03d [%s:%d] "fmt,day.tm_hour,day.tm_min,day.tm_sec,now.tv_nsec/1000000,__func__,__LINE__,##arg);    \
+}while(0)
+
+static void rainlog2file(const char *fmt,...)
 {
     int fd = openat(AT_FDCWD, RAINLILOG_FILENAME, O_CREAT|O_APPEND| O_WRONLY,0666);
     if( fd != -1 ){
@@ -34,6 +50,61 @@ void rainlog2file(const char *fmt,...)
     }
 }
 
+static void Backtrace(const char *fmt,...)
+{
+    int skip            = 1;
+    void *callstack[128];
+    const int nMaxFrames = sizeof(callstack) / sizeof(callstack[0]);
+    int nFrames         = backtrace(callstack, nMaxFrames);
+    char **symbols      = backtrace_symbols(callstack, nFrames);
+    va_list arg_ptr;
+    int i = skip;
+
+    int fd = openat(AT_FDCWD, RAINLILOG_FILENAME, O_CREAT|O_APPEND| O_WRONLY,0666);
+    if( fd == -1 ){
+        return ;
+    }    
+
+    va_start(arg_ptr, fmt);
+    vdprintf(fd,fmt,arg_ptr);
+    va_end(arg_ptr);
+
+    dprintf(fd,"backtrace:\n");
+
+    for (; i < nFrames; i++) {
+#if 0       //  simple mode,only print address
+        dprintf(fd,"\t<%s>\t", symbols[i]);
+#else       //  complex mode, try to resolve address name,need -ldl link option
+    Dl_info info;
+
+        if (dladdr(callstack[i], &info) && info.dli_sname) {
+            char *demangled = NULL;
+            int status = -1;
+#ifdef __cplusplus
+            if (info.dli_sname[0] == '_')
+                demangled = abi::__cxa_demangle(info.dli_sname, NULL, 0, &status);
+#endif
+            dprintf(fd, "%-3d %*p %s + %zd\n",
+                    i, (int)(2 + sizeof(void*) * 2), callstack[i],
+                    status == 0 ? demangled :
+                    info.dli_sname == 0 ? symbols[i] : info.dli_sname,
+                    (char *)callstack[i] - (char *)info.dli_saddr);
+            if( demangled )
+                free(demangled);
+#endif
+        } else {
+            dprintf(fd, "%-3d %*p %s\n",
+                    i, (int)(2 + sizeof(void*) * 2), callstack[i], symbols[i]);
+        }
+    }
+    free(symbols);
+    if (nFrames == nMaxFrames)
+        dprintf(fd, "[truncated]\n");
+    else
+        write(fd,"\n",1);
+    close(fd);
+}
+
 #ifdef __JAVA__
 void trace(){
     //Thread.currentThread().getStackTrace()
@@ -43,11 +114,20 @@ void trace(){
 }
 #endif
 
+
+int tsk_create(void *(*start_routine) (void *), void *arg)
+{
+    pthread_t slave_tid;
+    int ret = pthread_create(&slave_tid, NULL, start_routine, arg);
+    pthread_detach(slave_tid);
+    return ret;
+}
+
 static inline int file_recreate(const char *fname)
 {
     int fd;
-    unlinkat(AT_FDCWD,REMOTED_BOOTOK_NAME,0);
-    fd  = openat(AT_FDCWD,REMOTED_BOOTOK_NAME,O_CREAT,0666);
+    unlinkat(AT_FDCWD,fname,0);
+    fd  = openat(AT_FDCWD,fname,O_CREAT,0666);
     if( fd != -1 )
         close(fd);
     return fd != -1 ? 0: -1;
@@ -185,13 +265,13 @@ static int inotifyapi_readrec(struct inotify_handle *handle,int timeout_ms)
         handle->offset  += sizeof(struct inotify_event) + handle->ev->len;
         break;
     }
-
+    RAIN_DBGTRACE("get a rec:");
     return 0;
 }
 
 int main(int argc, char *argv[])  
 {  
-    const char              *target  = argc ==1 ? "." : argv[1];  
+    const char              *target  = (argc ==1) ? "." : argv[1];  
     struct inotify_handle   handle;
 
     int ret = inotifyapi_init(&handle, target,  IN_DELETE | IN_DELETE_SELF /*IN_ALL_EVENTS */);
@@ -203,12 +283,12 @@ int main(int argc, char *argv[])
     /* event:inotify_event -> name:char[event.len] */  
     while (1) {  
         ret = inotifyapi_readrec(&handle,0xffff);
-	if( ret < 0 ){
-	    if( ret == INOTIFYAPI_TIMEOUT )
-		continue;
-	    RAIN_DBGLOG("get inotify rec error:%d<%d>\n",ret,errno);
-	    break;
-	}
+        if( ret < 0 ){
+            if( ret == INOTIFYAPI_TIMEOUT )
+                continue;
+            RAIN_DBGLOG("get inotify rec error:%d<%d>\n",ret,errno);
+            break;
+        }
 
         struct inotify_event    *event = handle.ev;
 
