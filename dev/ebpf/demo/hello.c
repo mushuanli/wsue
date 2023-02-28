a6f8bc3b189cfc176710a8093b51a40761f2a6f6
RainLi <lizlok@gmail.com>
2023-02-28 16:18:30 +0800

    Create hello.c

---
+++ dev/ebpf/demo/hello.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+#include "hello.skel.h"
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+    return vfprintf(stderr, format, args);
+}
+
+int main(int argc, char **argv)
+{
+    struct hello_bpf *skel;
+    int err;
+
+    libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+    /* Set up libbpf errors and debug info callback */
+    libbpf_set_print(libbpf_print_fn);
+
+    /* Open BPF application */
+    skel = hello_bpf__open();
+    if (!skel) {
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        return 1;
+    }   
+
+    /* Load & verify BPF programs */
+    err = hello_bpf__load(skel);
+    if (err) {
+        fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+        goto cleanup;
+    }
+
+    /* Attach tracepoint handler */
+    err = hello_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach BPF skeleton\n");
+        goto cleanup;
+    }
+
+    printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
+           "to see output of the BPF programs.\n");
+
+    for (;;) {
+        /* trigger our BPF program */
+        fprintf(stderr, ".");
+        sleep(1);
+    }
+
+cleanup:
+    hello_bpf__destroy(skel);
+    return -err;
+}
